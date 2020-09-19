a46b2a9360291f21eec3115ee93ba668e0df6a75
Julian Miranda <71536893+JulianMi12@users.noreply.github.com>
2020-09-19 18:52:07 -0500

    Update RaisingBacteria.java

--- Codeforces/RaisingBacteria.java
+++ Codeforces/RaisingBacteria.java
@@ -1,4 +1,8 @@
-package raising.bacteria;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 /**
  *
@@ -6,10 +10,22 @@ package raising.bacteria;
  */
 public class RaisingBacteria {
 
-    
-    
-    
-    public static void main(String[] args) {
-        // TODO code application logic here
+    static BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter br = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) throws IOException {
+        long n = Long.parseLong(bf.readLine());
+        long bacteria = 0;
+        while (n >= 1) {
+            if (n % 2 == 1) {
+                bacteria++;
+                n--;
+            } else {
+                n /= 2;
+            }
+        }
+        br.write(String.valueOf(bacteria));
+        br.newLine();
+        br.close();
     }
 }
