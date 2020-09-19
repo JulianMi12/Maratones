ad3ad495028cf7b35f3df494f77c7854f3735676
Julian Miranda <71536893+JulianMi12@users.noreply.github.com>
2020-09-19 18:44:08 -0500

    Create RaisingBacteria.java

---
+++ Codeforces/RaisingBacteria.java
@@ -0,0 +1,15 @@
+package raising.bacteria;
+
+/**
+ *
+ * @author Julian_Miranda
+ */
+public class RaisingBacteria {
+
+    
+    
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
