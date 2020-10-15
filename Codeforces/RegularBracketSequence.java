5fcd32848b7a2669370b416c744e6022267756ae
Julian Miranda <71536893+JulianMi12@users.noreply.github.com>
2020-10-15 12:14:46 -0500

    Create RegularBracketSequence.java

---
+++ Codeforces/RegularBracketSequence.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Stack;
+
+/**
+ *
+ * @author Julian_Miranda
+ */
+public class RegularBracketSequence {
+
+    static BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter br = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) throws IOException {
+        String a = bf.readLine();
+        Stack bracket = new Stack();
+        int tot = 0;
+        for (int i = 0; i < a.length(); i++) {
+            if (a.charAt(i) == '(') {
+                bracket.push(a.charAt(i));
+            }
+            if (!bracket.isEmpty()) {
+                if (a.charAt(i) == ')' && bracket.peek().equals('(')) {
+                    bracket.pop();
+                    tot += 2;
+                }
+            }
+        }
+        System.out.println(tot);
+    }
+}
